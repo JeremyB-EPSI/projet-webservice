dd70112d98a418acd3a0c5598557072f68b6804f
Jeremy Beaupuy <jbeaupuy@actimeo.fr>
2017-04-27 17:34:14 +0200

    Submit a question to the server

--- src/app/general.service.ts
+++ src/app/general.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class GeneralService {
 
-  constructor() { }
+  API = 'http://localhost:3000';
+
+  constructor(private http: Http) { }
 
   sendQuestion (q: string) {
-    console.log(q);
+    this.http.post(`${this.API}/test`, {q}).subscribe(data => console.log(data));
   }
 
   getQuestion (id: number) {
